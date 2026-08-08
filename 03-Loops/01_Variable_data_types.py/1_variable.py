0e7b1748158b6014118a2f056cdedc79987f7fe7
AbdoSWEng <itabdo4tf@gmail.com>
2026-08-08 02:51:12 +0200

    Merge branch 'main' of https://github.com/AbdoSWEng/Python-Learning

---
+++ 03-Loops/01_Variable_data_types.py/1_variable.py
@@ -0,0 +1,13 @@
+name = "Ali"       # String
+age = 20           # Integer
+height = 1.75      # Float
+is_student = True  #Boolean
+print(name)
+print(age)
+print(height)
+print(is_student)
+# You can also check data type using type()SSS
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
